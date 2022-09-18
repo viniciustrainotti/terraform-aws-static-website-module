65037c669b50f0693913db10105a6d6b18517453
d3adb5 <me@d3adb5.net>
2022-09-18 16:50:04 -0300

    chore: remove unused local values
    Remove unused local values from `locals.tf`.

--- locals.tf
+++ locals.tf
@@ -1,6 +1,4 @@
 locals {
-  has_domain       = var.domain != ""
-  domain           = local.has_domain ? var.domain : ""
   website_filepath = var.website_path
   app_name_default = "${var.app_name}-${var.environment}"
 
